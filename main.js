10bf50d5e75564826213d46c42d49c6186259d00
squeex38 <squeex38@gmail.com>
2023-05-06 19:40:59 -0400

    add a search bar

--- main.js
+++ main.js
@@ -139,3 +139,15 @@ audioFiles.forEach(filename => {
 
 	CONTAINER.append(node);
 });
+
+const search = document.querySelector('input[type="search"]');
+search.addEventListener('input', e => {
+	const clips = Array.from(document.querySelectorAll('.audio-clip:not(#template)'));
+	clips.forEach(clip => clip.removeAttribute('hidden'));
+	clips.forEach(clip => {
+		const title = clip.querySelector('.label').innerText.toLowerCase();
+		if (!title.includes(search.value)) {
+			clip.setAttribute('hidden', '');
+		}
+	})
+});
